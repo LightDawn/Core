eef07fcd28bec900b226de834336a77453989455
Tabandeh <bahar23ir@gmail.com>
2014-08-30 15:26:30 +0430

    remove comment

--- Core.Mvc/Attribute/RegisterAttribute.cs
+++ Core.Mvc/Attribute/RegisterAttribute.cs
@@ -6,11 +6,7 @@ namespace Core.Mvc.Attribute
 {
     public  static class RegisterAttribute
     {
-        /// <summary>
-        //Created By:pouyan
-        //Date:1392/04/15
-        //Description:This Class Registers All Custom Attribute Validation ...It Calls In Application_Start
-        /// </summary>
+      
 
         public static void SetRegisterAttribute()
         {
