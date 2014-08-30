5d4a91404e990d59d0e939980e5ef5a2c19e3226
Tabandeh <bahar23ir@gmail.com>
2014-08-30 15:24:38 +0430

    add property for lookup on search

--- Core.Mvc/Attribute/Filter/SearchRelatedUponLoadByLookup.cs
+++ Core.Mvc/Attribute/Filter/SearchRelatedUponLoadByLookup.cs
@@ -6,20 +6,17 @@ using System.Threading.Tasks;
 
 namespace Core.Mvc.Attribute.Filter
 {
-    public class SearchRelatedUponLoadByLookupAttribute : System.Attribute 
+    public class SearchRelatedUponLoadByLookupAttribute : System.Attribute
     {
-
-        public SearchRelatedUponLoadByLookupAttribute(
-            string   lookUpName, 
-            string   lookupGridName , 
-            string   lookupRelatedId ,
-            string   lookupViewModel , 
-            string   iViewInfoProperty ,
-            string[] lookUpGridFor , 
-            string[] lookUpTreeFor , 
-            string[] isMultiSelectFor)
-        {
-            
-        }
+        // public string CustomType { get; set; }
+        public string PropertyNameForValue { get; set; }
+        public string PropertyNameForBinding { get; set; }
+        public string PropertyNameForDisplay { get; set; }
+        public string ViewModelPropName { get; set; }
+        public string Id { get; set; }
+        public string ViewModel { get; set; }
+        public string Title { get; set; }
+        //public string[] lookUpGridFor { get; set; }
+        //public string[] lookUpTreeFor { get; set; }//iViewInfoProperty , isMultiSelectFor
     }
 }
